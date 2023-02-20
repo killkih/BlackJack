7007e6f9b62fa69f07cba314f5e7556aa92cd0c0
Kuper <killkih@mail.ru>
2023-02-20 17:33:05 +0300

    BlackJack fixed v2

--- player.rb
+++ player.rb
@@ -2,6 +2,9 @@ class Player
   attr_accessor :money
   attr_reader :name, :cards
 
+  MAX_VALUE_ACE = 11
+  MIN_VALUE_ACE = 1
+
   def initialize(name)
     @name = name
     @money = 100
@@ -23,15 +26,19 @@ class Player
   def points
     points = 0
 
-    @cards.sort.each do |card|
+    cards.sort.each do |card|
       card_value = card.chop
 
       points += 10 if %w[J Q K].include?(card_value)
       points += card_value.to_i unless %w[J Q K T].include?(card_value)
       if card_value.include?('T') && points <= 10
-        points += 11
+        points += MAX_VALUE_ACE
       elsif card_value.include?('T') && points > 10
-        points += 1
+        if cards.include?('T')
+          points -= 9
+        else
+          points += MIN_VALUE_ACE
+        end
       end
     end
 
